8c255a256c15be588f7d51395b910e4935c33ab0
jeb2239 <jeb2239@columbia.edu>
2015-01-06 22:29:25 -0500

    testing out Xml reader in C#

---
+++ RSSReader/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.ServiceModel.Syndication;
+
+namespace RSSReader
+{
+    class Tester
+    {
+        public static void Main()
+        {
+
+            XmlReader readRss = XmlReader.Create("http://rss.cnn.com/rss/money_latest.rss");
+            SyndicationFeed syncfeed = SyndicationFeed.Load(readRss);
+            readRss.Close();
+            foreach (var item in syncfeed.Items)
+            {
+               String title= item.Title.Text;
+               Console.WriteLine(title);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
